359b702489ff4450bcde0a8ccb7979bfd17ba42c
aji-banyu <ajibanyurizqi@gmail.com>
2024-12-02 20:52:24 +0700

    first-commit-readMore

--- app.js
+++ app.js
@@ -12,4 +12,28 @@ document.addEventListener("click", (e) => {
     navbar.classList.remove("active");
   }
 });
-const hanabi = 
+
+// read more function
+const serviceItems = document.querySelector(".services-container");
+const popup = document.querySelector(".popup-box");
+const popupCloseBtn = document.querySelector(".popup-close-btn");
+const popupCloseIcon = document.querySelector(".popup-close-icon");
+
+serviceItems.addEventListener("click", (e) => {
+  if (e.target.tagName.toLowerCase() == "button") {
+    const item = e.target.parentElement;
+    const h3 = item.querySelector("h3").innerHTML;
+    const readMoreCont = item.querySelector(".read-more-cont").innerHTML;
+    popup.querySelector("h3").innerHTML = h3;
+    popup.querySelector(".popup-body").innerHTML = readMoreCont;
+
+    popupBox();
+  }
+});
+
+popupCloseBtn.addEventListener("click", popupBox);
+popupCloseIcon.addEventListener("click", popupBox);
+
+function popupBox() {
+  popup.classList.toggle("open");
+}
